1ae71b2c0bd4d0fe4252353ff5d406118c55529e
mohanson <mohanson@outlook.com>
2020-11-16 11:13:28 +0800

    Initial commit

---
+++ src/convention.rs
@@ -0,0 +1,2 @@
+pub const ISA: u8 = ckb_vm::ISA_IMAC | ckb_vm::ISA_B;
+pub const VERSION: u32 = ckb_vm::machine::VERSION1;
---
+++ src/main_interpreter64.rs
@@ -0,0 +1,27 @@
+use ckb_vm::SupportMachine;
+
+mod convention;
+mod cost_model;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    let code_data = std::fs::read(&args[1])?;
+    let code = bytes::Bytes::from(code_data);
+
+    let core_machine = ckb_vm::DefaultCoreMachine::<u64, ckb_vm::SparseMemory<u64>>::new(
+        convention::ISA,
+        convention::VERSION,
+        1 << 32,
+    );
+
+    let machine_builder = ckb_vm::DefaultMachineBuilder::new(core_machine)
+        .instruction_cycle_func(Box::new(cost_model::instruction_cycles));
+    let mut machine = machine_builder.build();
+    machine.load_program(&code, &vec!["main".into()]).unwrap();
+
+    let exit = machine.run();
+    let cycles = machine.cycles();
+    println!("int exit={:?} cycles={:?}", exit, cycles);
+
+    Ok(())
+}
