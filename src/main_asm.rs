6475fcd58e9652b0e602d96441191f9dab11ad65
Mohanson <mohanson@outlook.com>
2021-01-13 17:19:13 +0800

    Increase max cycles limit

--- src/main_asm.rs
+++ src/main_asm.rs
@@ -9,7 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let code = bytes::Bytes::from(code_data);
 
     let asm_core =
-        ckb_vm::machine::asm::AsmCoreMachine::new(convention::ISA, convention::VERSION, 1 << 32);
+        ckb_vm::machine::asm::AsmCoreMachine::new(convention::ISA, convention::VERSION, u64::MAX);
     let core =
         ckb_vm::DefaultMachineBuilder::<Box<ckb_vm::machine::asm::AsmCoreMachine>>::new(asm_core)
             .instruction_cycle_func(Box::new(cost_model::instruction_cycles))
